fe77a07d5a0451ba5cb07e52ef6a196baebba468
Sugam Kafle <95976724+sugamkafle@users.noreply.github.com>
2026-02-04 14:28:02 +0930

    Add HistoryManager class for managing history
    Implement HistoryManager class with history management.

--- Image Editor/history_manager.py
+++ Image Editor/history_manager.py
@@ -1,1 +1,10 @@
-
+class HistoryManager:
+    
+    # CLASS ATTRIBUTE
+    max_default_history = 20
+    
+    def __init__(self, max_history=None):
+        """CONSTRUCTOR with default parameter."""
+        self.__history = []  # ENCAPSULATION: Private list
+        self.__current_index = -1  # ENCAPSULATION: Private index
+        self.__max_history = max_history if max_history else HistoryManager.max_default_history
